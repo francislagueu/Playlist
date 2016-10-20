286269d425b9608faa9d43871c526224a0d7da41
jinisnotmyname <clyde.y.lee@gmail.com>
2016-10-20 10:27:28 -0700

    committing bryan's files to own branch

---
+++ routes/oauthflow.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+//grab the googleapi library and Oauth2 Object. 
+var googleapis = require('../custom_modules/google.js');
+var google = googleapis.getGoogle();
+var OAuth2 = google.auth.OAuth2;
+var scopes = ['https://www.googleapis.com/auth/youtube'];
+var oauth2Client = googleapis.getClient();
+
+function init(req,res,next){
+	//set the global oauth2 client for google.
+	var url = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope:scopes
+	});
+	res.redirect(url);
+}
+function grabAndSetCreds(req,res,next){
+	// grab the code from the query param
+	var code = req.query.code;
+	//get the access tokens. 
+	oauth2Client.getToken(code, function(err, tokens){
+		if(!err){
+			oauth2Client.setCredentials(tokens);
+			req.session.code = code;
+			google.options({auth:oauth2Client});
+			req.session.authorized = true;
+			res.redirect('../..');
+		}
+		else{
+			req.session.authorized = false;
+			next();
+		}
+	});
+}
+
+//start auth flow.
+router.get('/',init);
+//handle the callback url.
+router.get('/callback',grabAndSetCreds);
+
+module.exports = router;
