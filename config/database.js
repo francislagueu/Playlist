8149d89ade986632f1c70298571b69585766b15b
jinisnotmyname <clyde.y.lee@gmail.com>
2016-10-21 17:47:37 -0700

    auth, jdatabase, passport, user

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+	url:'monogodb://localhost:27012/users'
+	
+	}
---
+++ models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema{
+	{
+		//schema is a table
+		//create a table on the database
+		local:{
+			username:String
+			first_name:String,
+			last_name:String,
+			email:String,
+			password:String
+			
+		}
+	}
+};
+//generateHash is a custom name that francis created
+userSchema.methods.generateHash = function(password) {  
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+//bcryp methods are built-in
+
+//validpassword is a custom function name
+userSchema.methods.validPassword = function (password) 
+{
+	return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
