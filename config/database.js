4ae846777439245ccb4983e27ed6b8369624746b
francislagueu <lagson6@yahoo.fr>
2016-11-26 15:38:08 -0800

    Configure the db connection string to differentiate when in development or in production

--- config/database.js
+++ config/database.js
@@ -1,5 +1,10 @@
 module.exports = {
-	url: 'mongodb://<dbuser>:<dbpassword>@ds019638.mlab.com:19638/users'
+	production: {
+        url: 'mongodb://playlistmanager:PlaylistManager1234*@ds019638.mlab.com:19638/users'
+    },
 
+    development:{
+		url:'mongodb://localhost:27017/users'
+	}
 }
 
