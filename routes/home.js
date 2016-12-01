4882afbb5e86c4ef0af8cdadbec0e6f00d448def
Kaz-adr <akzadr@gmail.com>
2016-12-01 14:43:48 -0800

    added thumbnails to spotify playlist

--- routes/home.js
+++ routes/home.js
@@ -157,7 +157,8 @@ router.get('/spotplaylist?', function(req,res,next){
 			for(var i=0; i < tracks.length; i++){
 				var item = {
 					'id': tracks[i].track.id,
-					'name': tracks[i].track.name
+					'name': tracks[i].track.name,
+					'albumart': tracks[i].track.album.images[0].url
 				};
 				playlist['items'][i] = item;
 			}
